ac3a01bcd582730791c8d732187004a0d54a9886
se-ma2024 <Sei.2024.IT@gmail.com>
2023-12-28 01:30:17 +0900

    SearchBarのUI作成 検索機能は未実装

---
+++ app/src/main/java/com/example/foodsearch/SearchScreen/SearchBar.kt
@@ -0,0 +1,73 @@
+package com.example.foodsearch.SearchScreen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Close
+import androidx.compose.material.icons.sharp.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.foodsearch.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    var SearchWord by remember { mutableStateOf("") }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = SearchWord,
+            onValueChange = {
+                SearchWord = it
+            },
+            label = { Text(stringResource(R.string.SearchWord)) },
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            leadingIcon = {
+                Icon(
+                    Icons.Sharp.Search,
+                    contentDescription = null
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    Icons.Sharp.Close,
+                    contentDescription = "×",
+                    modifier = Modifier.clickable {
+                        SearchWord = ""
+                    }
+                )
+            },
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+fun PreSearchBar() {
+    SearchBar()
+}
